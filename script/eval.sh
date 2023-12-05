9eb381cd6df60954a29c541d49fac3fc40c14276
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-04 23:36:23 -0600

    Update eval.sh

--- script/eval.sh
+++ script/eval.sh
@@ -1,6 +1,6 @@
 GPU=$1
 data=$2
-CUDA_VISIBLE_DEVICES=${GPU} python -u benchmark_summarization.py \
+CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_summarization.py \
 --model-name state-spaces/mamba-2.8b \
 --input_path ${data} \
 --sample_num 100

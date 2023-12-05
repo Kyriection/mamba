738905ad1722f30fd9f591f6a14dc4cc921878d9
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-05 02:06:39 -0600

    Update eval_pythia.sh

--- script/eval_pythia.sh
+++ script/eval_pythia.sh
@@ -1,8 +1,20 @@
+# GPU=2
+# for data in xsum cnn_dailymail; do
+# for shots in 0 3 5; do
+# CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_summarization.py \
+# --model-name EleutherAI/pythia-2.8b-v0 \
+# --input_path data/${data}_${shots}shot.jsonl \
+# --sample_num 200
+# done
+# done
+
+
+
 GPU=2
 for data in xsum cnn_dailymail; do
 for shots in 0 3 5; do
 CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_summarization.py \
---model-name EleutherAI/pythia-2.8b-v0 \
+--model-name openlm-research/open_llama_3b \
 --input_path data/${data}_${shots}shot.jsonl \
 --sample_num 200
 done

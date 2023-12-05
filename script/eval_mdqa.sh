5f72a8b9ddf0c45baafd4e1ca2e80d2d3d9d4c19
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-05 02:07:47 -0600

    Update eval_mdqa.sh

--- script/eval_mdqa.sh
+++ script/eval_mdqa.sh
@@ -1,9 +1,21 @@
 
-GPU=$1
-idx=$2
+# GPU=$1
+# idx=$2
+# CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
+#     --model-name state-spaces/mamba-2.8b \
+#     --answer_idx ${idx} \
+#     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
+#     --sample_num 200
+# python -u benchmarks/eval_qa_response.py --input-path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl
+
+
+
+GPU=0
+for idx in 1 3 5 7 10; do
 CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
-    --model-name state-spaces/mamba-2.8b \
+    --model-name EleutherAI/pythia-2.8b-v0 \
     --answer_idx ${idx} \
     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
     --sample_num 200
 python -u benchmarks/eval_qa_response.py --input-path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl
+done

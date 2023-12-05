642f848df61e37ed563cb4a3c8b91d4c48c2d836
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-05 16:25:08 -0600

    upload

--- script/eval_mdqa.sh
+++ script/eval_mdqa.sh
@@ -21,10 +21,21 @@
 # done
 
 
+# GPU=1
+# for idx in 1 3 5 7 10; do
+# CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
+#     --model-name openlm-research/open_llama_3b \
+#     --answer_idx ${idx} \
+#     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
+#     --sample_num 200
+# python -u benchmarks/eval_qa_response.py --input-path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl
+# done
+
+
 GPU=1
 for idx in 1 3 5 7 10; do
 CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
-    --model-name openlm-research/open_llama_3b \
+    --model-name RWKV/rwkv-5-world-3b \
     --answer_idx ${idx} \
     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
     --sample_num 200

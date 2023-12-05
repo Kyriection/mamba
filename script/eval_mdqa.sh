662fdabb7b75c0a1950be3ada436c76581256df4
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-05 02:09:36 -0600

    Update eval_mdqa.sh

--- script/eval_mdqa.sh
+++ script/eval_mdqa.sh
@@ -10,10 +10,21 @@
 
 
 
-GPU=0
+# GPU=0
+# for idx in 1 3 5 7 10; do
+# CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
+#     --model-name EleutherAI/pythia-2.8b-v0 \
+#     --answer_idx ${idx} \
+#     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
+#     --sample_num 200
+# python -u benchmarks/eval_qa_response.py --input-path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl
+# done
+
+
+GPU=1
 for idx in 1 3 5 7 10; do
 CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_mdqa.py \
-    --model-name EleutherAI/pythia-2.8b-v0 \
+    --model-name openlm-research/open_llama_3b \
     --answer_idx ${idx} \
     --output_path qa_results/MDQA_${idx}answer-mamba_2.8b.jsonl \
     --sample_num 200

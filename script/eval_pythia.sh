f77cb405a7b385aebda82c38bf1c7f87f85190c6
Allen <39230030+Kyriection@users.noreply.github.com>
2023-12-05 18:03:55 -0600

    Update eval_pythia.sh

--- script/eval_pythia.sh
+++ script/eval_pythia.sh
@@ -22,9 +22,20 @@
 
 
 
+# GPU=2
+# for data in xsum cnn_dailymail; do
+# for shots in 0 3 5; do
+# CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_summarization.py \
+# --model-name RWKV/rwkv-5-world-3b \
+# --input_path data/${data}_${shots}shot.jsonl \
+# --sample_num 200
+# done
+# done
+
+
 GPU=2
-for data in xsum cnn_dailymail; do
-for shots in 0 3 5; do
+for data in xsum; do
+for shots in 0; do
 CUDA_VISIBLE_DEVICES=${GPU} python -u benchmarks/benchmark_summarization.py \
 --model-name RWKV/rwkv-5-world-3b \
 --input_path data/${data}_${shots}shot.jsonl \
